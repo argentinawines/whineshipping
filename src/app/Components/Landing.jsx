60afe60d00af695b1bc1819c04772fd303dd1c5e
Francisco Lugo <tu_correo@ejemplo.com>
2025-12-15 14:15:54 -0300

    Fix Landing logo src

--- src/app/Components/Landing.jsx
+++ src/app/Components/Landing.jsx
@@ -1,37 +1,31 @@
-import React from "react";
-import banner from "../../assets/portada.svg";
 import Image from "next/image";
-
+import banner from "../../assets/portada.svg";
 
 function Landing() {
   return (
-   <div id="main" >
-  <section id="one" className="relative w-full">
-    {/* Logotipo */}
-    <img
-      src="http://vinos.sistemas4b.com/wp-content/uploads/2020/04/fhdhd-03.webp"
-      alt="Flowbite Logo"
-      className="
-      
-        absolute 
-        top-2 left-2 
-        h-9 sm:h-10 md:h-14
-        z-20 
-        bg-black bg-opacity-50 
-        p-1
-      "
-    />
+    <div id="main">
+      <section id="one" className="relative w-full">
+        {/* Logo local */}
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={160}
+          height={56}
+          priority
+          className="absolute top-2 left-2 z-20 bg-black/50 p-1 h-9 sm:h-10 md:h-14 w-auto"
+        />
 
-    {/* Imagen Banner */}
-    <div className="w-full">
-      <Image 
-        src={banner} 
-        alt="Banner" 
-        className="w-full object-cover max-h-[80vh] h-auto block" 
-      />
+        {/* Banner */}
+        <div className="w-full">
+          <Image
+            src={banner}
+            alt="Banner"
+            priority
+            className="w-full object-cover max-h-[80vh] h-auto block"
+          />
+        </div>
+      </section>
     </div>
-  </section>
-</div>
   );
 }
 
